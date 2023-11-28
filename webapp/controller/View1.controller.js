6c43d8b22a7a28f9c50be76fbe212aeb4fff1792
Sreevani Kamalla <sreevani.kamalla04@gmail.com>
2023-11-28 09:50:25 +0000

    existing

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -10,6 +10,22 @@ sap.ui.define([
         return Controller.extend("com.sap.bankapp.controller.View1", {
             onInit: function () {
 
+            },
+            //creating dialog
+            onOpenBankDetails: function () {
+                if (!this.moreBankDetails) {
+                    this.moreBankDetails = this.loadFragment(
+                        { name: "com.sap.bankapp.view.fragments.MoreDetails" }
+                    );
+                }
+                this.moreBankDetails.then(
+                    function (oDialog) {
+                        oDialog.open();
+                    });
+
+            },
+            onCloseBankDetails: function () {
+                this.byId("moreBankDetails").close();
             }
         });
     });
